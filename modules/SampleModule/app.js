eb8c1e2897108cce6a15817ca87bdd90beb5cfbf
Jens Saalmüller <jens.saalmueller@codecentric.de>
2021-02-05 12:10:42 +0100

    initial commit

---
+++ modules/SampleModule/app.js
@@ -0,0 +1,72 @@
+'use strict';
+
+var Transport = require('azure-iot-device-mqtt').Mqtt;
+var Client = require('azure-iot-device').ModuleClient;
+var Message = require('azure-iot-device').Message;
+
+Client.fromEnvironment(Transport, function (err, client) {
+  if (err) {
+    throw err;
+  } else {
+    client.on('error', function (err) {
+      throw err;
+    });
+
+    // connect to the Edge instance
+    client.open(function (err) {
+      if (err) {
+        throw err;
+      } else {
+        console.log('IoT Hub module client initialized');
+
+        // Act on input messages to the module.
+        client.on('inputMessage', function (inputName, msg) {
+          pipeMessage(client, inputName, msg);
+        });
+      }
+    });
+  }
+});
+
+// This function just pipes the messages without any change.
+function pipeMessage(client, inputName, msg) {
+  client.complete(msg, printResultFor('Receiving message'));
+
+  if (inputName === 'input1') {
+    var message = msg.getBytes().toString('utf8');
+    if (message) {
+      var outputMsg = new Message(message);
+      client.sendOutputEvent('output1', outputMsg, printResultFor('Sending received message'));
+    }
+  }
+}
+
+// Helper function to print results in the console
+function printResultFor(op) {
+  return function printResult(err, res) {
+    if (err) {
+      console.log(op + ' error: ' + err.toString());
+    }
+    if (res) {
+      console.log(op + ' status: ' + res.constructor.name);
+    }
+  };
+}
+
+setInterval(function () {
+  // Simulate telemetry.
+  var temperature = 20 + (Math.random() * 15);
+  var message = new Message(JSON.stringify({
+    temperature: temperature,
+    humidity: 60 + (Math.random() * 20)
+  }));
+
+  // Add a custom application property to the message.
+  // An IoT hub can filter on these properties without access to the message body.
+  message.properties.add('temperatureAlert', (temperature > 30) ? 'true' : 'false');
+
+  console.log('Sending temperature message: ' + message.getData());
+
+  // Send the message.
+  client.sendOutputEvent('temperature', message, printResultFor('Sending temperature'));
+}, 5000);
